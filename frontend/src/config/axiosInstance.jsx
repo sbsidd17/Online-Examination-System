f9b36543eb0206540a749945a5a6a2fdfc881991
Shuaib Siddiqui <sbsidd17@gmail.com>
2023-12-14 12:56:54 +0530

    uodate cors

--- frontend/src/config/axiosInstance.jsx
+++ frontend/src/config/axiosInstance.jsx
@@ -1,14 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
-const axiosInstance = axios.create({
-  withCredentials: true,
-  baseURL: BASE_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  credentials: "include",
-});
-
-export default axiosInstance;
+export const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true
+})
