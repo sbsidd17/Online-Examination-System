1522d066212dfc238dbf0234ffc0c24f4377ebdb
Shuaib Siddiqui <sbsidd17@gmail.com>
2023-12-14 13:25:36 +0530

    updated axiosInstance

--- frontend/src/config/axiosInstance.jsx
+++ frontend/src/config/axiosInstance.jsx
@@ -4,7 +4,10 @@ const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
 })
 
 export default axiosInstance
