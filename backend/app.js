955a0f6e8fd041f3a56538c479117dd8252a43f9
Shuaib Siddiqui <sbsidd17@gmail.com>
2023-12-14 14:01:10 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,8 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: [process.env.FRONTEND_URL], credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.options('*', cors());
 app.get("/",(req,res)=>{
     res.send("Backend Is Running...")
 })
